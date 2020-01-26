7d264150c38ed94f0cd7ea126339a333b5ff93fc
Brian <brindang776@aol.com>
2020-01-25 20:05:50 -0500

    refactor of sequential models into functional models; started MNIST demo

---
+++ examples/mnist_gtn.py
@@ -0,0 +1,77 @@
+from kerasgtn.gtn import GTN
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Activation, Flatten, Reshape
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from tensorflow.keras.layers import LeakyReLU, Dropout
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import Adadelta, Adam, RMSprop
+
+class MNIST_GTN(GTN):
+    def __init__(self, **kwargs):
+        super(MNIST_GTN, self).__init__(**kwargs)
+    
+    def get_generator(self, input_layer):
+        if self.generator is not None:
+            return self.generator
+        dropout = 0.4
+        depth = 256
+        dim = 7
+        # In: (100,) 
+        # Out: dim x dim x depth
+        x = Dense(dim*dim*depth)(input_layer)
+        x = BatchNormalization(momentum=0.9)(x)
+        x = Activation('relu')(x)
+        x = Reshape((dim, dim, depth))(x)
+        x = Dropout(dropout)(x)
+
+        # In: dim x dim x depth
+        # Out: 2*dim x 2*dim x depth/2
+        x = UpSampling2D()(x)
+        x = Conv2DTranspose(int(depth/2), 5, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
+        x = Activation('relu')(x)
+
+        x = UpSampling2D()(x)
+        x = Conv2DTranspose(int(depth/4), 5, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
+        x = Activation('relu')(x)
+
+        x = Conv2DTranspose(int(depth/8), 5, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
+        x = Activation('relu')(x)
+
+        # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
+        x = Conv2DTranspose(1, 5, padding='same')(x)
+        x = Activation('sigmoid')(x)
+        self.generator = x
+        return self.generator
+
+    def get_learner(self, input_layer):
+        if self.learner is not None:
+            print(input_layer)
+            return self.learner
+        
+        x = Conv2D(64, (3, 3), padding='same')(input_layer)
+        x = LeakyReLU()(x)
+        x = Dropout(0.2)(x)
+        
+        x = Conv2D(64, (3, 3), padding='same')(x)
+        x = LeakyReLU()(x)
+        x = Dropout(0.2)(x)
+
+        x = Conv2D(64, (3, 3), padding='same')(x)
+        x = LeakyReLU()(x)
+        x = Dropout(0.2)(x)
+        
+        x = Flatten()(x)
+
+        x = Dense(256, activation='relu')(x)
+        x = Dense(256, activation='relu')(x)
+        self.learner = x
+        return self.learner
+
+
+if __name__ == "__main__":
+    gtn = MNIST_GTN(real_input_shape=(28, 28, 1), n_classes=10)
+    model = gtn.get_model()
+    
--- kerasgtn/gtn.py
+++ kerasgtn/gtn.py
@@ -11,18 +11,18 @@ from tensorflow.keras.optimizers import Adadelta, Adam, RMSprop
 
 
 class GTN(object):
-    def __init__(self, real_input_shape, n_classes, noise_shape=(100,), optimizer=None):
+    def __init__(self, real_input_shape, n_classes, noise_shape=None, optimizer=None):
 
         # shape of the real data without batch size
-        self.input_shape = input_shape
+        self.real_input_shape = real_input_shape
 
         # number of classes
         self.n_classes = n_classes
 
         # shape of the noise vector for the generator
-        self.noise_shape = noise_shape
+        self.noise_shape = noise_shape if noise_shape else (100,)
         
-        # optimizer
+        # optimizer for both inner and outer loops...
         self.optimizer = RMSprop() if optimizer is None else optimizer
 
         # populated with functions below
@@ -30,54 +30,21 @@ class GTN(object):
         self.generator = None
         self.model = None
 
-    def get_learner(self):
+    def get_learner(self, input_layer):
         """
-        :return: an uncompiled keras.models.Sequential model that represents a learner
+        :return: an uncompiled keras.models.Model that represents a learner
         """
         if self.learner:
             return self.learner
         raise NotImplementedError
 
-    def get_generator(self):
+    def get_generator(self, input_layer):
         """
-        A template generator, feel free to overwrite to meet needs.
-        :return: an uncompiled keras.models.Sequential model that represents a generator
+        :return: an uncompiled keras.models.Model model that represents a generator
         """
         if self.generator:
             return self.generator
-        self.generator = Sequential()
-        dropout = 0.4
-        depth = 512
-        dim = 7
-        # In: 100
-        # Out: dim x dim x depth
-        self.generator.add(Dense(dim*dim*depth, input_dim=100))
-        self.generator.add(BatchNormalization(momentum=0.9))
-        self.generator.add(Activation('relu'))
-        self.generator.add(Reshape((dim, dim, depth)))
-        self.generator.add(Dropout(dropout))
-
-        # In: dim x dim x depth
-        # Out: 2*dim x 2*dim x depth/2
-        self.generator.add(UpSampling2D())
-        self.generator.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
-        self.generator.add(BatchNormalization(momentum=0.9))
-        self.generator.add(Activation('relu'))
-
-        self.generator.add(UpSampling2D())
-        self.generator.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
-        self.generator.add(BatchNormalization(momentum=0.9))
-        self.generator.add(Activation('relu'))
-
-        self.generator.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
-        self.generator.add(BatchNormalization(momentum=0.9))
-        self.generator.add(Activation('relu'))
-
-        # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
-        self.generator.add(Conv2DTranspose(1, 5, padding='same'))
-        self.generator.add(Activation('sigmoid'))
-        self.generator.summary()
-        return self.generator
+        raise NotImplementedError
 
     def get_model(self):
         """
@@ -90,15 +57,15 @@ class GTN(object):
         # ('outer loop' from the GTN paper)
         # real training data
         real_input = Input(shape=self.real_input_shape, name='real_input')
-        real_learner = self.get_learner()(real_input)
+        real_learner = self.get_learner(real_input)
         real_output = Dense(self.n_classes, activation='sigmoid', name='real_output')(real_learner)
 
         # ('inner loop' from the GTN paper)
         # noise input
-        fake_input = Input(shape=self.noise_shape, name='fake_input'))
-        teacher = self.get_generator()(fake_input)
-        fake_learner = self.get_learner()(teacher)
-        fake_output = Dense(self.n_classes, activation='sigmoid', name='fake_output')(teacher)
+        fake_input = Input(shape=self.noise_shape, name='fake_input')
+        teacher = self.get_generator(fake_input)
+        fake_learner = self.get_learner(teacher)(teacher)
+        fake_output = Dense(self.n_classes, activation='sigmoid', name='fake_output')(fake_learner)
 
         self.model = Model(
             inputs=[real_input, fake_input],
@@ -112,10 +79,10 @@ class GTN(object):
                 'real_output': 'categorical_crossentropy',
                 'fake_output': 'categorical_crossentropy'
             },
-            loss_weights=[
+            loss_weights={
                 'real_output': 1.0,
                 'fake_output': 0.01
-            ]
+            }
         )
         return self.model
 
